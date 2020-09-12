276b972be5d1117ca1f57af81ed07437e470110a
DatNguyen <bkdn.ntdat@gmail.com>
2020-09-12 11:17:45 +0700

    Slide Show

---
+++ Slide Show/index.js
@@ -0,0 +1,25 @@
+﻿var images = [];
+
+function MyImage(name, description) {
+    this.Name = name;
+    this.Description = description;
+}
+
+images.push(new MyImage("A", "Hoa"));
+images.push(new MyImage("B", "Tháp Eiffel"));
+images.push(new MyImage("C", "Ruộng bậc thang"));
+images.push(new MyImage("D", "Nuôi trồng thủy sản"));
+images.push(new MyImage("E", "Ruộng bậc thang"));
+images.push(new MyImage("F", "Núi"));
+
+var imgs_display = document.getElementsByClassName("img-list")
+
+var count = 0
+var interval = setInterval(function () {
+    document.getElementById("img").src = "./Image/" + images[count].Name + ".jpeg";
+    document.getElementById("description").textContent = images[count++].Description
+    if (count == imgs_display.length) {
+        clearInterval(interval);
+        console.log("Dừng vòng lặp rồi nhá!")
+    }
+}, 1000)
